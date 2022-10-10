b05eaeef76b8387f0becb41ad1e504a2b6c8c6e7
karan137 <karan.dssorot@gmail.com>
2022-10-09 22:13:42 -0700

    Progress1

---
+++ SER515_DesignPattern/src/Login.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Login {
+
+    public Login(){
+        Scanner cin = new Scanner(System.in);
+        System.out.println("Enter Username");
+        String username = cin.next();
+        System.out.println("Enter Password");
+        String password = cin.next();
+
+    }
+}
