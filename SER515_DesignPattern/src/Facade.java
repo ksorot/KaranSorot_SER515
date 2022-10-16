f6f154d5eab726990bab823293c04805f1b9102f
ksorot <kasorot@asu.edu>
2022-10-15 20:10:04 -0700

    Progress 5 Facade& Visitor Pattern

--- SER515_DesignPattern/src/Facade.java
+++ SER515_DesignPattern/src/Facade.java
@@ -1,3 +1,8 @@
+import sun.awt.geom.AreaOp;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Facade {
     private int UserType;
 
@@ -14,7 +19,28 @@ public class Facade {
 
     public void facadehandler(){
     Login loginobj = new Login();
-    int userType = loginobj.Logingin();
+        int userType = loginobj.Logingin();
+    while(userType == -1){
+        System.out.println("Invalid Username or Password, Please Try Again");
+        userType = loginobj.Logingin();
+    }
+        int input = 2 ;
+        while(input != 0 && input != 1){
+            System.out.println("Select Product from the following options :");
+            System.out.println("Enter 0 for Meat Product");
+            System.out.println("Enter 1 for Produce Product");
+            Scanner systemin = new Scanner(System.in);
+            input = systemin.nextInt();
+
+            if (input == 0) {
+                nProductCategory = 0;
+            } else if (input == 1) {
+                nProductCategory = 1;
+            } else {
+                System.out.println(" Invalid Input, please select again from 1 or 0");
+            }
+        }
+        System.out.println("VISITOR PATTERN");
     }
 
     public void addTrading() {
